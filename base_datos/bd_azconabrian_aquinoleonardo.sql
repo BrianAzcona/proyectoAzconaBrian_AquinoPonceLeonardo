dc2ca89f1f08b4c64548ba0afbb6863d590766d1
laxtu16 <leoaquino445@gmail.com>
2025-06-05 11:43:44 -0300

    nueva base de datos

--- base_datos/bd_azconabrian_aquinoleonardo.sql
+++ base_datos/bd_azconabrian_aquinoleonardo.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 18-05-2025 a las 05:52:03
+-- Tiempo de generación: 05-06-2025 a las 16:42:37
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -24,6 +24,28 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `juego_categoria`
+--
+
+CREATE TABLE `juego_categoria` (
+  `categoria_id` int(255) NOT NULL,
+  `categoria_descripcion` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tab_categoria`
+--
+
+CREATE TABLE `tab_categoria` (
+  `categoria_id` int(255) NOT NULL,
+  `categoria_descripcion` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `tab_clientes`
 --
 
@@ -39,6 +61,32 @@ CREATE TABLE `tab_clientes` (
   `cliente_ciudad` text NOT NULL,
   `perfil_id` int(11) NOT NULL,
   `cliente_telefono` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `tab_clientes`
+--
+
+INSERT INTO `tab_clientes` (`cliente_id`, `cliente_nombre`, `cliente_apellido`, `cliente_dni`, `cliente_correo`, `cliente_password`, `cliente_pais`, `cliente_provincia`, `cliente_ciudad`, `perfil_id`, `cliente_telefono`) VALUES
+(1, 'ceci', 'colman', 46458891, 'ceci@gmail.com', '$2y$10$J3ym4KUFqR7Cd2yk61MXhenHj4DqtGSS.CsNEhhP0qCM2RmrsQ5Dq', 'Argentina', 'corrientes', 'corrientes', 2, 2147483647),
+(2, 'Brian', 'Azcona', 45759787, 'brianazcona@gmail.com', '$2y$10$N/tElA/4htQZtXJbdreJU.80PuEdpiCvA9/JfYkjxdso43boPLY1C', 'Argentina', 'co', 'corrientes', 1, 2147483647);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tab_juegos`
+--
+
+CREATE TABLE `tab_juegos` (
+  `juego_id` int(50) NOT NULL,
+  `juego_nombre` varchar(255) NOT NULL,
+  `juego_plataforma` varchar(255) NOT NULL,
+  `juego_descripcion` text NOT NULL,
+  `juego_stock` int(255) NOT NULL,
+  `juego_precio` decimal(65,0) NOT NULL,
+  `juego_imagen` varchar(255) NOT NULL,
+  `categoria_id` int(255) NOT NULL,
+  `juego_estado` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -74,6 +122,18 @@ CREATE TABLE `tab_perfil` (
 --
 
 --
+-- Indices de la tabla `juego_categoria`
+--
+ALTER TABLE `juego_categoria`
+  ADD PRIMARY KEY (`categoria_id`);
+
+--
+-- Indices de la tabla `tab_categoria`
+--
+ALTER TABLE `tab_categoria`
+  ADD PRIMARY KEY (`categoria_id`);
+
+--
 -- Indices de la tabla `tab_clientes`
 --
 ALTER TABLE `tab_clientes`
@@ -96,10 +156,22 @@ ALTER TABLE `tab_perfil`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `juego_categoria`
+--
+ALTER TABLE `juego_categoria`
+  MODIFY `categoria_id` int(255) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tab_categoria`
+--
+ALTER TABLE `tab_categoria`
+  MODIFY `categoria_id` int(255) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de la tabla `tab_clientes`
 --
 ALTER TABLE `tab_clientes`
-  MODIFY `cliente_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `cliente_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `tab_mensaje`
